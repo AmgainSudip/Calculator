d769a4d2a8caf3bc6c27e48dc823e0c1a52a5049
Sudip Amgain <sudipamgain2061@gamil.com>
2026-02-02 01:15:50 -0500

    create event listeners to get the input

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let userInp = "";
+
 function add(num1, addOpe, num2) {
     let sum = 0;
-    sum += num1 + num2;
-    return sum; 
+    sum = parseInt(num1) + parseInt(num2);
+    console.log(sum);
+    displayResult(sum);
 }
 
 function subtract(num1, subOpe, num2) {
@@ -17,12 +20,77 @@ function divide(a,divOpe,b) {
 
 }
 
-function operate(num1, num2, operator) {
-
+function factorial(num) {
 
 }
 
+function operate(operator) {
+    if (operator == "+") {
+        let nums = userInp.split("+");
+        add(nums[0], operator, nums[1]);
+    }
+
+    else if (operator == "-") {
+        let nums = userInp.split("-");
+        subtract(nums[0], operator, nums[1]);
+    }
+
+    else if (operator == "×") {
+        let nums = userInp.split("×");
+        multiply(nums[0], operator, nums[1]);
+    }
+
+    else if (operator == "÷") {
+        let nums = userInp.split("÷");
+        divide(nums[0], operator, nums[1]);
+    }
+
+    else if (operator == "!") {
+        let num = userInp.split("!");
+        factorial(num);
+    }
+}
 
 function updateNum() {
-
 }
+
+const btn = document.querySelectorAll("button");
+const box = document.querySelector("#box");
+const clear = document.querySelector("#clear");
+const equal = document.querySelector("#equal");
+
+let operator = "";
+btn.forEach(button => {
+    if (button.textContent == "+" || button.textContent == "-" || button.textContent == "×" || button.textContent == "÷") {
+        button.addEventListener("click", (event) => {
+            display(button.textContent);})
+            operator = button.textContent;
+    }
+
+    else if (button.textContent != "Clear" && button.textContent != "=") {
+        button.addEventListener("click", (event) => {
+            display(button.textContent); 
+    });}
+    console.log(operator)
+});
+
+clear.addEventListener("click", () => {
+    userInp = "";
+    box.innerHTML = "";
+});
+
+
+function display(input) {
+    userInp += input;
+    box.innerHTML = userInp;
+}
+
+function displayResult(result) {
+    box.innerHTML = "";
+    box.innerHTML = result;
+    userInp = "";
+}
+
+equal.addEventListener("click", () =>
+    operate(operator)
+);
