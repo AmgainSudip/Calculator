8170c85666a04b662c60c1df465f2810e17014ac
Sudip Amgain <sudipamgain2061@gamil.com>
2026-02-01 22:59:50 -0500

    add visual calculator not functioning yet

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(num1, addOpe, num2) {
+    let sum = 0;
+    sum += num1 + num2;
+    return sum; 
+}
+
+function subtract(num1, subOpe, num2) {
+    return num1- num2;
+}
+
+function multiply(a, mulOpe, b) {
+    return a*b;
+}
+
+function divide(a,divOpe,b) {
+    return a/b;
+
+}
+
+function operate(num1, num2, operator) {
+
+
+}
+
+
+function updateNum() {
+
+}
